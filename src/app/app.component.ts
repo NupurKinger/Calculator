8f0f3c7d424072333ffcdd8b2044b93903e35d36
Nupur Kinger <nupurkinger@gmail.com>
2020-03-04 16:21:55 +0700

    Commit app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'calculator';
+  title = 'Calculator App';
+  a: number;
+  b: number;
+  result: number;
+
+  add() {
+    this.result = this.a + this.b;
+  }
+
+  subtract() {
+    this.result = this.a - this.b;
+  }
+
+  multiply() {
+    this.result = this.a * this.b;
+  }
+
+  divide() {
+    this.result = this.a / this.b;
+  }
 }
